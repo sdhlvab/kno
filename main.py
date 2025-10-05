a635c81882be2a3bccb52c8f6497bd6f225a2181
kamilsuchomski <ksuchomski@hotmail.com>
2025-10-05 20:39:40 +0200

    added plot

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import argparse
 from PIL import Image
 import sys
+import matplotlib.pyplot as plt
 
 
 def main() -> int:
@@ -12,14 +13,19 @@ def main() -> int:
 
     model = tf.keras.models.load_model("model.keras")
 
-    # mnist = tf.keras.datasets.mnist
-    # (x_train, y_train),(x_test, y_test) = mnist.load_data()
-    # x_train, x_test = x_train / 255.0, x_test / 255.0
-    # model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28, 28)),   tf.keras.layers.Dense(128, activation='relu'),   tf.keras.layers.Dropout(0.2),   tf.keras.layers.Dense(10, activation='softmax') ])
-    # model.compile(optimizer='adam',               loss='sparse_categorical_crossentropy',               metrics=['accuracy'])
-    # model.fit(x_train, y_train, epochs=5) # użyj verbose=0 jeśli jest problem z konsolą
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train),(x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+    model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28, 28)),   tf.keras.layers.Dense(128, activation='relu'),   tf.keras.layers.Dropout(0.2),   tf.keras.layers.Dense(10, activation='softmax') ])
+    model.compile(optimizer='adam',               loss='sparse_categorical_crossentropy',               metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=5) # użyj verbose=0 jeśli jest problem z konsolą
     # model.evaluate(x_test, y_test)
     # model.save("model.keras")
+
+    plt.plot(model.history.history['accuracy'])
+    plt.plot(model.history.history['loss'])
+    plt.show()
+
     print(f"plik wejsciowy: {args.plik}")
 
     img = Image.open(args.plik).convert("L")
